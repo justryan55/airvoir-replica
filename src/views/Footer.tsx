b7f58d0dcf08db6e308086b3487f60ddcb187a90
Ryan <ryan.irani@gmail.com>
2025-03-31 10:46:52 +0100

    increase space between line and contact

--- src/views/Footer.tsx
+++ src/views/Footer.tsx
@@ -165,6 +165,7 @@ const LineDivider = styled.div`
   background-color: var(--border-color--border-secondary);
   width: 100%;
   height: 1px;
+  margin-top: 20px;
 `;
 
 const TextSmallV2 = styled(TextSmall)`
@@ -204,7 +205,7 @@ export default function Footer() {
           <RightColumn>
             <RightSubContainer>
               <List>
-                <ListHeader>Pages</ListHeader>
+                {/* <ListHeader>Pages</ListHeader> */}
                 <Item>Home</Item>
                 <Item>Book a flight</Item>
                 <Item>About</Item>
@@ -212,8 +213,19 @@ export default function Footer() {
                 <Item>Contact</Item>
               </List>
               <List>
-                <ListHeader>Socials</ListHeader>
-                <Item>LinkedIn</Item>
+                {/* <ListHeader>Socials</ListHeader> */}
+                <Item>
+                  <svg viewBox="0 0 128 128" height="40" width="40">
+                    <path
+                      fill="#0076b2"
+                      d="M116 3H12a8.91 8.91 0 00-9 8.8v104.42a8.91 8.91 0 009 8.78h104a8.93 8.93 0 009-8.81V11.77A8.93 8.93 0 00116 3z"
+                    ></path>
+                    <path
+                      fill="#fff"
+                      d="M21.06 48.73h18.11V107H21.06zm9.06-29a10.5 10.5 0 11-10.5 10.49 10.5 10.5 0 0110.5-10.49M50.53 48.73h17.36v8h.40c2.42-4.58 8.32-9.41 17.13-9.41C103.6 47.28 107 59.35 107 75v32H88.89V78.65c0-6.75-.12-15.44-9.41-15.44s-10.87 7.36-10.87 15V107H50.53z"
+                    ></path>
+                  </svg>
+                </Item>
               </List>
             </RightSubContainer>
           </RightColumn>
